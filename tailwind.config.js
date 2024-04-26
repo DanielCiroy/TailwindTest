016cf2e8b5ce79825231bb144b99ac7cfaff94cc
Daniel ciroy <142291477+DanielCiroy@users.noreply.github.com>
2024-04-26 10:39:58 +0200

    exo site skynet avec tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'black': '#000000',
+      'white': '#ffffff',
+      'blue1': '#148fce',
+      'blue2': '#093951',
+
+    },
+
+
     extend: {},
   },
   plugins: [],
